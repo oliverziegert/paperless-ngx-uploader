e10b0b38e45aaef79cb8ef87b5a8472cb3bf493c
Oliver Ziegert <oliver@pc-ziegert.de>
2026-02-03 21:35:06 +0100

    make fmt happy again

--- src/cmd/client/helpers.rs
+++ src/cmd/client/helpers.rs
@@ -12,10 +12,7 @@ pub(crate) const MAX_TITLE_LENGTH: usize = 64;
 /// The title is the filename without its extension, truncated to a maximum length of
 /// `MAX_TITLE_LENGTH`. This is used as the title when uploading the file to Paperless-ngx.
 pub(crate) fn get_title_from_filename(file: &std::path::Path) -> String {
-    let name = file
-        .file_stem()
-        .and_then(|s| s.to_str())
-        .unwrap_or("");
+    let name = file.file_stem().and_then(|s| s.to_str()).unwrap_or("");
     // Truncate the name to a maximum length of `MAX_TITLE_LENGTH`.
     // This is a safety precaution to avoid overflowing the Paperless-ngx title field.
     name.split_at(min(name.len(), MAX_TITLE_LENGTH)).0.to_string()
