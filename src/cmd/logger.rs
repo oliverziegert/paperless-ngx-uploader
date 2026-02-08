21a7cbac0448ccabc5633032f3f5cc3076527d4b
Oliver Ziegert <oliver@pc-ziegert.de>
2026-02-08 16:15:39 +0100

    auto-claude: subtask-1-1 - Change default log level from LevelFilter::Error to LevelFilter::Info

--- src/cmd/logger.rs
+++ src/cmd/logger.rs
@@ -10,7 +10,7 @@ pub fn setup_logging(verbose: u8) {
 
     // Set global log level based on the verbose flag
     match verbose {
-        0 => log_builder.filter_level(LevelFilter::Error),
+        0 => log_builder.filter_level(LevelFilter::Info),
         1 => log_builder.filter_level(LevelFilter::Warn),
         2 => log_builder.filter_level(LevelFilter::Info),
         3 => log_builder.filter_level(LevelFilter::Debug),
